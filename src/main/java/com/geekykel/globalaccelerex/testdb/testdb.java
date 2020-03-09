7bb6f1a0499b82231a542830bd9041ce7952b77c
Kelvinator07 <kelvinator4leo@gmail.com>
2020-03-09 14:32:36 +0100

    Implemented database configurations.

---
+++ src/main/java/com/geekykel/globalaccelerex/testdb/testdb.java
@@ -0,0 +1,27 @@
+package com.geekykel.globalaccelerex.testdb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class testdb {
+
+    public static void main(String[] args) {
+
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/globalaccelerex?useSSL=false&serverTimezone=GMT+1";
+        String user = "postgres";
+        String pass = "12345";
+
+        try {
+
+            System.out.println("Connecting to database: " + jdbcUrl);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            System.out.println("Connection successful!!!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
