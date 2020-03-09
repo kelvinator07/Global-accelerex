0c44c3031962c855112ac586578d85486881ff5b
Kelvinator07 <kelvinator4leo@gmail.com>
2020-03-09 14:22:26 +0100

    Initial commit

---
+++ src/main/java/com/geekykel/globalaccelerex/GlobalAccelerexApplication.java
@@ -0,0 +1,13 @@
+package com.geekykel.globalaccelerex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalAccelerexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalAccelerexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekykel/globalaccelerex/GlobalAccelerexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekykel.globalaccelerex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalAccelerexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
